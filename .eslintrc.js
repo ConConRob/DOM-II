40e44d46d1553e7ad4481a66de79548068318391
conconrob <connortprobertson@gmail.com>
2019-01-29 12:15:36 +0000

    init commit

---
+++ .eslintrc.js
@@ -0,0 +1,12 @@
+module.exports = {
+    "extends": "airbnb-base",
+    "env": {
+        "browser": true,
+        "node": true
+    },
+    "rules": {
+        "linebreak-style": 0,
+        "quote-props": 0,
+        "quotes": 0,
+    }
+};
