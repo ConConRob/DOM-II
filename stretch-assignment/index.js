2ad718f67cd05bba1d9b4465a20e505dfa8cf806
conconrob <connortprobertson@gmail.com>
2019-01-29 21:00:09 +0000

    added not realistic gravity

--- stretch-assignment/index.js
+++ stretch-assignment/index.js
@@ -4,20 +4,32 @@ const blockGreen = document.querySelector('.block--green');
 const blockPink = document.querySelector('.block--pink');
 const blockGray = document.querySelector('.block--gray');
 const blocks = document.querySelectorAll('.block');
+// variables
 let lowestOrder = 0;
 const vertGain = -30;
+const vertTime = 0.5;
+const gravityTime = 5;
+const gravityDist = 0.1;
+// functions
 function goUp(event) {
   const { target } = event;
-
-  TweenMax.to(target, 1, { y: vertGain + target.verticalDistance });
+  TweenMax.to(target, vertTime, { y: vertGain + target.verticalDistance });
   target.verticalDistance += vertGain;
-  console.log('going up');
 }
+
 blocks.forEach((block) => {
   block.verticalDistance = 0;
   block.horizontalDistance = 0;
-  console.log(block);
+  //
   block.addEventListener('click', goUp);
+  // ADD SOME GRAVITY
+  setInterval(() => {
+    TweenMax.to(block, vertTime, { y: gravityDist + block.verticalDistance });
+    block.verticalDistance += gravityDist;
+    if (block.verticalDistance > 0) {
+      block.verticalDistance = 0;
+    }
+  }, gravityTime);
 });
 // TweenMax.from('.nav', 2, { x: 700 });
 // TweenMax.from('.logo-heading', 2, { x: -700 });
